d786519c5d5559494043ac720440285201eb8ea3
Bjarte Stien Karlsen <bjarte@bjartek.org>
2011-03-06 18:39:33 +0100

    Implemented xml quiz provider, console game and swing game

--- quiz/src/main/scala/scalaexamples/quiz/console/ConsoleGame.scala
+++ quiz/src/main/scala/scalaexamples/quiz/console/ConsoleGame.scala
@@ -1,11 +1,34 @@
 package scalaexamples.quiz.console
 
 import scalaexamples.quiz.{Answer, Question, Quiz}
+import annotation.tailrec
 
 class ConsoleGame(quiz: Quiz) {
-
   println("Welcome to our faboulus quiz called '" + quiz.title + "'")
 
-  // TODO: Implement quiz game UI and logic
-  
+  val answers = quiz.questions.map {
+    question =>
+      println("Question: " + question.text);
+      question.answers.zipWithIndex.foreach {case (answer, index) => println((index + 1) + ". " + answer.text)}
+      println("What is your answer?:")
+      askQuestion(question)
+  }
+
+  val correct = answers.filter(_.correct == true).size
+
+  println("You answered correctly on  " + correct + " of " + quiz.questions.size + " questions::")
+
+  @tailrec
+  private def askQuestion(question: Question): Answer = {
+    def parseInput = try readLine.toInt - 1 catch {case _ => -1}
+    val index = parseInput
+    //lazy val input = try readLine.toInt - 1 catch {case _ => -1}
+
+    if (!question.answers.isDefinedAt(index)) {
+      println("Not valid input, try again!:")
+      askQuestion(question)
+    } else {
+      question.answers(index)
+    }   
+  }
 }
